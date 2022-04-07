93b47227e25c1bc42abebc6ed568ecf3fb9ee47e
dungpv <dungpv@novaon.vn>
2022-04-07 10:59:10 +0700

    init project

---
+++ src/AspdotnetCommonPackages/DateTimeExtensions/Convert.DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspdotnetCommonPackages
+{
+    /// <summary>
+    /// Every thing you need when convert to DateTime
+    /// </summary>
+    public static partial class DatetimeExtensions
+    {
+        public static Boolean IsValidDateTimeString(this string input)
+        {
+
+        }
+        public static DateTime FromString(this string input)
+        {
+
+        }
+    }
+}
---
+++ src/AspdotnetCommonPackages/NullExtensions/Common.NullExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AspdotnetCommonPackages
+{
+    public static partial class NullExtensions
+    {
+        public static Boolean IsNull<T>(this T source){
+            if (source == null)
+                return true;
+
+            return false;
+        }
+
+        public static Boolean IsNotNull<T>(this T source)
+        {
+            return source.IsNull() ? false : true;
+        }
+
+        public static Boolean IsNotNullAndHaveData<T>(this ICollection<T> sources)
+        {
+            if (sources.IsNull())
+                return false;
+
+            return sources.Count() > 0;
+        }
+
+        public static T GetDefaultValueIfNull<T>(this T source, T defaultValue = default(T))
+        {
+            if (source.IsNull())
+                return defaultValue;
+
+            return source;
+        }
+
+    }
+}
