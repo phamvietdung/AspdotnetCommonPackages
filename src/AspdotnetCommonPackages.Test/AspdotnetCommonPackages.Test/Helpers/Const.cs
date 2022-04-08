f3017b156cb70cf4f13d69f3478ce33b4fd040a2
dungpv <dungpv@novaon.vn>
2022-04-08 14:14:17 +0700

    update unittest

--- src/AspdotnetCommonPackages.Test/AspdotnetCommonPackages.Test/Helpers/Const.cs
+++ src/AspdotnetCommonPackages.Test/AspdotnetCommonPackages.Test/Helpers/Const.cs
@@ -15,5 +15,12 @@ namespace AspdotnetCommonPackages.Test.Helpers
         public static string DateTimeWrongFormat = "2022/13/20";
 
         public static string DateTimeCorrectFormat = "2022/05/22";
+
+        public static string UnicodeString = "Thễ qứick brớwn fỡx jumps õver thế lãzỷ đỗg";
+
+        public static string AsiiString = "The quick brown fox jumps over the lazy dog";
+
+        public static string AsiiAliasString = "The_quick_brown_fox_jumps_over_the_lazy_dog";
+
     }
 }
---
+++ src/AspdotnetCommonPackages.Test/AspdotnetCommonPackages.Test/UnitTestTextExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AspdotnetCommonPackages.Test.Helpers;
+using System.Linq;
+
+namespace AspdotnetCommonPackages.Test
+{
+    [TestClass]
+    public class UnitTestTextExtensions
+    {
+
+        [TestMethod]
+        public void ReplaceASII_Test()
+        {
+            string input = Const.UnicodeString;
+
+            Assert.AreEqual(input.ReplaceUnicodeCharacterByTheASIICharacter(), Const.AsiiString.ToLower());
+
+            Assert.AreEqual(input.ReplaceUnicodeCharacterByTheASIICharacter(false), Const.AsiiString.ToUpper());
+
+            Assert.AreEqual(input.ToAlias(), Const.AsiiAliasString.ToLower());
+
+            Assert.AreEqual(input.ToAlias(false), Const.AsiiAliasString.ToUpper());
+        }
+
+        [TestMethod]
+        public void Masking_Test()
+        {
+            string input = "ndklfnbkldfnbldknfbldkfnbldkfnbldkfnbldknfblkdnfblkdnflbknowieioncwiencopwienvskldnlvkndflkbnoeinbd8";
+
+            string maskingCharacter = "*";
+
+            int hiddenRatio = 98;
+
+            string masking = input.Masking(maskingCharacter, hiddenRatio);
+
+            Assert.IsTrue(masking.ToCharArray().Select(s => s.ToString()).Count(c => c == maskingCharacter) >= hiddenRatio);
+        }
+
+    }
+}
