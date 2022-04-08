c29921b1f0adf608f24ca2238b21dcbfc52af177
dungpv <dungpv@novaon.vn>
2022-04-08 14:13:15 +0700

    update UnitTest

---
+++ src/AspdotnetCommonPackages/TextExtensions/Common.TextExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AspdotnetCommonPackages
+{
+    public static partial class TextExtensions
+    {
+        
+        public static string ToAlias(this string source, bool convertToLowerCase = true, string alternativeCharacter = "_")
+        {
+            return source
+                .ReplaceUnicodeCharacterByTheASIICharacter(convertToLowerCase)
+                .ReplaceSpecialCharacterByUnderLineCharacter(alternativeCharacter);
+        }
+
+        public static string ReplaceUnicodeCharacterByTheASIICharacter(this string source, bool convertToLowerCase = true)
+        {
+            if (source.IsNull())
+                return String.Empty;
+
+            string[] chars = source.ToCharArray().Select(s => s.ToString().ToLower()).ToArray();
+
+            Dictionary<string, string> alternative = GetAlternativeList();
+
+            for(int i = 0; i < chars.Length; i++)
+            {
+                foreach(var pair in alternative)
+                {
+                    if (pair.Value.IndexOf(chars[i]) >= 0)
+                        chars[i] = pair.Key;
+                }
+            }
+
+            return String.Join("", convertToLowerCase ? chars.Select(s => s.ToLower()) : chars.Select(s =>s.ToUpper()));
+
+        }
+
+        public static string ReplaceSpecialCharacterByUnderLineCharacter(this string source, string alternativeCharacter = "_")
+        {
+            source = Regex.Replace(source, specialRegexNeedReplace, alternativeCharacter);
+
+            source = Regex.Replace(source, specialRegexNeedRemove, String.Empty);
+
+            return source;
+
+        }
+
+        /*
+        * Masking some character in a string with alternative
+        * character. Default is "*" but it can be change to
+        * another. Make sure allways masking/hidden about 50%
+        * to 60% information.
+        */
+        public static string Masking(this string source, string alternative_character = "*", int ratioHidden = 60)
+        {
+            if (source == null)
+                return String.Empty;
+
+            var total = source.Length;
+
+            var total_masking = total * ratioHidden / 100;
+
+            var start_at = (total - total_masking) / 2;
+
+            var masking = "";
+
+            for (var i = 0; i < total_masking; i++)
+                masking += alternative_character;
+
+            return source.Substring(0, start_at) + masking + source.Substring(start_at + total_masking, total - (start_at + total_masking));
+
+        }
+    }
+}
---
+++ src/AspdotnetCommonPackages/TextExtensions/Private.TextExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AspdotnetCommonPackages
+{
+    public static partial class TextExtensions
+    {
+        private static string specialRegexNeedReplace = @"\s|\-";
+        private static string specialRegexNeedRemove = @"\!|\@|\#|\$|\%|\^";
+        /// <summary>
+        /// Unicode
+        /// https://en.wikipedia.org/wiki/List_of_Unicode_characters
+        /// 
+        /// ASII
+        /// https://www.w3schools.com/charsets/ref_html_ascii.asp
+        /// </summary>
+        private static Dictionary<string, string[]> alternativeCharacterList = new Dictionary<string, string[]>
+        {
+           {"a",new string[]{"á", "à", "ả", "ã", "ạ", "â", "ấ", "ầ", "ẩ", "ẫ", "ậ", "ă", "ắ", "ằ", "ẳ", "ẵ", "ặ"}},
+           {"d",new string[]{"đ"}},
+           {"e",new string[]{"é","è","ẻ","ẽ","ẹ","ê","ế","ề","ể","ễ","ệ"}},
+           {"i",new string[]{"í","ì","ỉ","ĩ","ị"}},
+           {"o",new string[]{"ó","ò","ỏ","õ","ọ","ô","ố","ồ","ổ","ỗ","ộ","ơ","ớ","ờ","ở","ỡ","ợ"}},
+           {"u",new string[]{"ú","ù","ủ","ũ","ụ","ư","ứ","ừ","ử","ữ","ự"}},
+           {"y",new string[]{"ý","ỳ","ỷ","ỹ","ỵ"}},
+        };
+
+        private static Dictionary<string, string> GetAlternativeList()
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            foreach (var pair in alternativeCharacterList)
+            {
+                result.Add(pair.Key, String.Join("", pair.Value));
+            }
+
+            return result;
+        }
+
+        private static string[] GetAlternativeCharacterList()
+        {
+            string[] result = new string[alternativeCharacterList.Sum(s => s.Value.Count())];
+
+            int index = 0; // start node
+
+            foreach(var pair in alternativeCharacterList)
+            {
+                for(var i = 0; i < pair.Value.Length; i++)
+                {
+                    result[index] = pair.Value[i];
+
+                    index++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
